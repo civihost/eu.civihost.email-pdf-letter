c505003469d857f5e2d9601c9bff03e268324953
Samuele Masetto <samuele.masetto@gmail.com>
2022-01-17 18:36:49 +0100

    update to 5.45.1

--- CRM/EmailPdfLetter/Hook/Hook.php
+++ CRM/EmailPdfLetter/Hook/Hook.php
@@ -1,6 +1,7 @@
 <?php
 
 use CRM_EmailPdfLetter_ExtensionUtil as E;
+use Civi\Token\TokenProcessor;
 
 class CRM_EmailPdfLetter_Hook_Hook
 {
@@ -13,119 +14,97 @@ class CRM_EmailPdfLetter_Hook_Hook
      */
     public function alterMailParams(&$params, $context)
     {
-        $templateVars = CRM_Core_Smarty::singleton()->get_template_vars();
-        //print_r($templateVars['contact']);
-        //print_r($templateVars);
-        //exit();
-        if (isset($_POST['html_message2']) && isset($_POST['email_options']) && isset($templateVars['contact'])) {
+        print_r($params);
+        print_r($context);
+        print_r($_POST['email_options']);
+
+        if (isset($_POST['html_message2']) && isset($_POST['email_options']) && isset($params['contact_id'])) {
             if (stristr($_POST['email_options'], 'pdfemail') && $params['html'] == ts('Please see attached')) {
+                echo "test";
                 $html_message = $_POST['html_message2'];
                 $realSeparator = ', ';
                 $tableSeparators = [
-                  'td' => '</td><td>',
-                  'tr' => '</td></tr><tr><td>',
+                    'td' => '</td><td>',
+                    'tr' => '</td></tr><tr><td>',
                 ];
                 $separator = '****~~~~';
                 //the original thinking was mutliple options - but we are going with only 2 (comma & td) for now in case
                 // there are security (& UI) issues we need to think through
                 if (isset($_POST['group_by_separator'])) {
-                  if (in_array($_POST['group_by_separator'], ['td', 'tr'])) {
-                    $realSeparator = $tableSeparators[$_POST['group_by_separator']];
-                  }
-                  elseif ($_POST['group_by_separator'] == 'br') {
-                    $realSeparator = "<br />";
-                  }
+                    if (in_array($_POST['group_by_separator'], ['td', 'tr'])) {
+                        $realSeparator = $tableSeparators[$_POST['group_by_separator']];
+                    } elseif ($_POST['group_by_separator'] == 'br') {
+                        $realSeparator = "<br />";
+                    }
                 }
                 $groupBy = $_POST['group_by'];
                 $messageToken = CRM_Utils_Token::getTokens($html_message);
                 $grouped = FALSE;
                 $groupByID = 0;
                 if ($groupBy) {
-                  $grouped = TRUE;
+                    $grouped = TRUE;
                 }
-                /*
-                $result = civicrm_api3('Contribution', 'get', [
-                    'sequential' => 1,
-                    'id' => ['IN' => [123, 1234]],
-                  ]);
-                $result = civicrm_api3('Contribution', 'get', [
-                    'sequential' => 1,
-                    'id' => ['IN' => [30847, 28577]],
-                  ]);
-                */
+
                 $contributions = [];
                 $contribution = null;
 
-                $contact = $templateVars['contact'];
-
-                //$html_message = CRM_Contribute_Form_Task_PDFLetterCommon::generateHtml($contact, $contribution, $groupBy, $contributions, $realSeparator, $tableSeparators, $messageToken, $html_message, $separator, $grouped, $groupByID);
-                $html_message = str_replace($separator, $realSeparator, self::resolveTokens($html_message, $contact, $contribution, $messageToken, $grouped, $separator));
+                $html_message = str_replace(
+                    $separator,
+                    $realSeparator,
+                    self::resolveTokens($html_message, $params['contact_id'], null, $grouped, $separator, [])
+                );
 
                 $params['html'] = $html_message;
-
             }
         }
-        /*
-    if ($params['valueName'] == 'contribution_invoice_receipt') {
-      $templateVars = CRM_Core_Smarty::singleton()->get_template_vars();
-
-      if (!empty($templateVars['payment_processor_type'])
-        && $templateVars['payment_processor_type'] != 'PostalPayment')
-      {
-        return;
-      }
-
-      try {
-        $messageTemplateId = civicrm_api3('MessageTemplate', 'getvalue', [
-          'sequential' => 1,
-          'return' => 'id',
-          'msg_title' => CRM_Postalpayment_Install_MsgTemplate::MSG_TITLE,
-        ]);
-      } catch (CiviCRM_API3_Exception $e) {}
-
-      if(empty($messageTemplateId)) {
-        return;
-      }
-
-      $params['messageTemplateID'] = (int) $messageTemplateId;
-
-      $domain = CRM_Core_BAO_Domain::getDomain();
-      $customValue = civicrm_api3('CustomValue', 'get', [
-        'entity_id' => $domain->contact_id,
-        'format.field_names' => 1,
-      ]);
-      CRM_Core_Smarty::singleton()->assign('domain_iban', str_split($customValue['values']['iban'][0]));
-    }
-      */
     }
 
-    private static function resolveTokens($html_message, $contact, $contribution, $messageToken, $grouped, $separator) {
-        $categories = self::getTokenCategories();
-        $domain = CRM_Core_BAO_Domain::getDomain();
-        $tokenHtml = CRM_Utils_Token::replaceDomainTokens($html_message, $domain, true, $messageToken);
-        $tokenHtml = CRM_Utils_Token::replaceContactTokens($tokenHtml, $contact, true, $messageToken);
+    private static function resolveTokens(string $html_message, int $contactID, $contributionID, $grouped, $separator, $contributions): string
+    {
+        $tokenContext = [
+            'smarty' => (defined('CIVICRM_MAIL_SMARTY') && CIVICRM_MAIL_SMARTY),
+            'contactId' => $contactID,
+            'schema' => ['contributionId'],
+        ];
         if ($grouped) {
-            $tokenHtml = CRM_Utils_Token::replaceMultipleContributionTokens($separator, $tokenHtml, $contribution, true, $messageToken);
-        } else {
-            // no change to normal behaviour to avoid risk of breakage
-            $tokenHtml = CRM_Utils_Token::replaceContributionTokens($tokenHtml, $contribution, true, $messageToken);
+            // First replace the contribution tokens. These are pretty ... special.
+            // if the text looks like `<td>{contribution.currency} {contribution.total_amount}</td>'
+            // and there are 2 rows with a currency separator of
+            // you wind up with a string like
+            // '<td>USD</td><td>USD></td> <td>$50</td><td>$89</td>
+            // see https://docs.civicrm.org/user/en/latest/contributions/manual-receipts-and-thank-yous/#grouped-contribution-thank-you-letters
+            $tokenProcessor = new TokenProcessor(\Civi::dispatcher(), $tokenContext);
+            $contributionTokens = CRM_Utils_Token::getTokens($html_message)['contribution'] ?? [];
+            foreach ($contributionTokens as $token) {
+                $tokenProcessor->addMessage($token, '{contribution.' . $token . '}', 'text/html');
+            }
+
+            foreach ($contributions as $contribution) {
+                $tokenProcessor->addRow([
+                    'contributionId' => $contribution['id'],
+                    'contribution' => $contribution,
+                ]);
+            }
+            $tokenProcessor->evaluate();
+            $resolvedTokens = [];
+            foreach ($contributionTokens as $token) {
+                foreach ($tokenProcessor->getRows() as $row) {
+                    $resolvedTokens[$token][$row->context['contributionId']] = $row->render($token);
+                }
+                $html_message = str_replace('{contribution.' . $token . '}', implode($separator, $resolvedTokens[$token]), $html_message);
+            }
         }
-        $tokenHtml = CRM_Utils_Token::replaceHookTokens($tokenHtml, $contact, $categories, true);
-        if (defined('CIVICRM_MAIL_SMARTY') && CIVICRM_MAIL_SMARTY) {
-            $smarty = CRM_Core_Smarty::singleton();
-            // also add the tokens to the template
-            $smarty->assign_by_ref('contact', $contact);
-            $tokenHtml = $smarty->fetch("string:$tokenHtml");
-        }
-        return $tokenHtml;
+        $tokenContext['contributionId'] = $contributionID;
+        return CRM_Core_TokenSmarty::render(['html' => $html_message], $tokenContext)['html'];
     }
 
-      private static function getTokenCategories() {
+    private static function getTokenCategories()
+    {
         if (!isset(Civi::$statics[__CLASS__]['token_categories'])) {
-          $tokens = [];
-          CRM_Utils_Hook::tokens($tokens);
-          Civi::$statics[__CLASS__]['token_categories'] = array_keys($tokens);
+            $tokens = [];
+            CRM_Utils_Hook::tokens($tokens);
+            Civi::$statics[__CLASS__]['token_categories'] = array_keys($tokens);
         }
         return Civi::$statics[__CLASS__]['token_categories'];
-      }
+    }
 }
