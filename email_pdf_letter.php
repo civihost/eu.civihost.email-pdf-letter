8060ae98b1e73c802c3d9cef5c05b8b3e2b68bbe
Samuele Masetto <samuele.masetto@gmail.com>
2025-02-11 10:23:10 +0100

    civix upgrade

--- email_pdf_letter.php
+++ email_pdf_letter.php
@@ -14,16 +14,6 @@ function email_pdf_letter_civicrm_config(&$config)
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_xmlMenu
- */
-function email_pdf_letter_civicrm_xmlMenu(&$files)
-{
-    _email_pdf_letter_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_install
@@ -31,26 +21,6 @@ function email_pdf_letter_civicrm_xmlMenu(&$files)
 function email_pdf_letter_civicrm_install()
 {
     _email_pdf_letter_civix_civicrm_install();
-}
-
-/**
- * Implements hook_civicrm_postInstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_postInstall
- */
-function email_pdf_letter_civicrm_postInstall()
-{
-    _email_pdf_letter_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_uninstall
- */
-function email_pdf_letter_civicrm_uninstall()
-{
-    _email_pdf_letter_civix_civicrm_uninstall();
 }
 
 /**
@@ -63,98 +33,6 @@ function email_pdf_letter_civicrm_enable()
     _email_pdf_letter_civix_civicrm_enable();
 }
 
-/**
- * Implements hook_civicrm_disable().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_disable
- */
-function email_pdf_letter_civicrm_disable()
-{
-    _email_pdf_letter_civix_civicrm_disable();
-}
-
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_upgrade
- */
-function email_pdf_letter_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL)
-{
-    return _email_pdf_letter_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_managed
- */
-function email_pdf_letter_civicrm_managed(&$entities)
-{
-    _email_pdf_letter_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_caseTypes
- */
-function email_pdf_letter_civicrm_caseTypes(&$caseTypes)
-{
-    _email_pdf_letter_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_angularModules
- */
-function email_pdf_letter_civicrm_angularModules(&$angularModules)
-{
-    _email_pdf_letter_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_alterSettingsFolders
- */
-function email_pdf_letter_civicrm_alterSettingsFolders(&$metaDataFolders = NULL)
-{
-    _email_pdf_letter_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_entityTypes
- */
-function email_pdf_letter_civicrm_entityTypes(&$entityTypes)
-{
-    _email_pdf_letter_civix_civicrm_entityTypes($entityTypes);
-}
-
-/**
- * Implements hook_civicrm_thems().
- */
-function email_pdf_letter_civicrm_themes(&$themes)
-{
-    _email_pdf_letter_civix_civicrm_themes($themes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -162,9 +40,8 @@ function email_pdf_letter_civicrm_themes(&$themes)
  *
  * @link https://docs.civicrm.org/dev/en/latest/hooks/hook_civicrm_preProcess
  *
-function email_pdf_letter_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
